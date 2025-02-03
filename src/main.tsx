d38f57d15fc7323191487356c5c0f4396af4e38e
Christian Stewart <christian@aperture.us>
2025-02-03 12:11:11 -0800

    feat: add react-scan
    Signed-off-by: Christian Stewart <christian@aperture.us>

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,16 @@
+import { scan } from "react-scan";
+import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 
-createRoot(document.getElementById("root")!).render(<App />);
+if (typeof window !== "undefined") {
+  scan({
+    enabled: true,
+    log: true, // logs render info to console (default: false)
+  });
+}
+
+createRoot(document.getElementById("root")!).render(
+  (<App />) as React.ReactNode,
+);
